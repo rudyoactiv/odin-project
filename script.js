1cf3aba75298dca568ec34e622afa9418e72e26a
Rudraneel Dutta <its.rudraneel@gmail.com>
2025-05-02 23:40:31 +0530

    responsive host

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function loadProject(path) {
-    document.getElementById('preview').src = path;
+  document.getElementById('preview').src = path;
+
+  const sidebar = document.getElementById('sidebar');
+  if (sidebar.classList.contains('open')) {
+    sidebar.classList.remove('open');
   }
-  
+}
+
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  document.getElementById('sidebar').classList.toggle('open');
+});
